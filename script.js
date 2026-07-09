ac53409a0054f5a87756c192c00c56319648bf36
Tafajjul Ansari <tafajjulans141@gmail.com>
2026-07-09 12:25:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,121 @@
+let allVideos=[];
+
+
+fetch("videos.json")
+.then(r=>r.json())
+.then(data=>{
+    
+    
+allVideos=data;
+
+show(data);
+
+});
+
+
+function show(list){
+
+const box=document.getElementById("videos");
+
+box.innerHTML="";
+
+list.forEach((v,i)=>{
+
+box.innerHTML+=`
+
+<div class="card">
+
+<img src="thumbnail/${v.thumbnail}">
+
+<div class="info">
+
+<h3>${v.title}</h3>
+
+<p>${v.description}</p>
+
+<a class="watch" href="watch.html?id=${v.id}">
+▶ Watch
+</a>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+}
+
+
+document.getElementById("search").addEventListener("input",function(){
+
+let text=this.value.toLowerCase();
+
+show(
+
+allVideos.filter(v=>
+
+v.title.toLowerCase().includes(text) ||
+
+v.description.toLowerCase().includes(text)
+
+)
+
+);
+
+});
+// Splash animation complete hone ke baad fade out
+
+setTimeout(() => {
+    document.getElementById("splash-screen").style.opacity = "0";
+}, 2200);
+window.addEventListener("load", () => {
+
+    const splash = document.getElementById("splash-screen");
+
+    // 2.3 sec baad splash hide
+    setTimeout(() => {
+
+        splash.classList.add("hide");
+
+        // Animation complete hone ke baad remove
+        setTimeout(() => {
+            splash.remove();
+        }, 600);
+
+    }, 2300);
+
+});
+const search = document.getElementById("search");
+
+function searchVideos(){
+    const text = search.value.toLowerCase();
+
+    show(
+        allVideos.filter(v =>
+            v.title.toLowerCase().includes(text) ||
+            v.description.toLowerCase().includes(text)
+        )
+    );
+}
+
+search.addEventListener("input", searchVideos);
+document.getElementById("searchBtn").addEventListener("click", searchVideos);
+
+document.getElementById("searchBtn").onclick = () => {
+    searchVideos();
+    document.getElementById("search").blur();
+};
+
+const searchBox = document.querySelector(".search-box");
+const searchInput = document.getElementById("search");
+
+searchInput.addEventListener("focus", () => {
+    searchBox.classList.add("active");
+});
+
+searchInput.addEventListener("blur", () => {
+    if(searchInput.value.trim()==="")
+        searchBox.classList.remove("active");
+});
